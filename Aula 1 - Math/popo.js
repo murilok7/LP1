963913c3537f64f341b6c7bb0173c0d27b49d526
murilok7 <munak72008@gmail.com>
2024-09-02 08:25:18 -0300

    aprendi sobre os matematicos haha

---
+++ Aula 1 - Math/popo.js
@@ -0,0 +1,29 @@
+let numero = 4.7
+
+//Math.round arredonda os numeros
+console.log(Math.round(numero))
+
+//Math.ceil arredonda sempre para cima
+console.log(Math.ceil(numero))
+
+//Math.floor sempre arrendonda para baixo
+console.log(Math.floor(numero))
+
+//Math.pow gera potenciação
+console.log(Math.pow(5, 2))
+
+//Math.sqrt raiz quadrada
+console.log(Math.sqrt(25))
+
+//Math.abs gera o numero absoluto postivo
+console.log(Math.abs(-5))
+
+//Math.min encontra o menor numero
+console.log(Math.min(7, 45, 89, 2, 125))
+
+//Math.max encontra o maior numero
+console.log(Math.max(7, 45, 89, 2, 125))
+
+let Numaleatorio = Math.floor(Math.random() * 10)
+
+console.log(Numaleatorio)
